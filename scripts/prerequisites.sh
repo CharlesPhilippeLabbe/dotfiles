b305385a20a9e9632f82147a87c61adbdbf1cd5e
Charles-Philippe <c.p.labbe@me.com>
2025-04-09 21:29:49 -0400

    Update prerequisites.sh remove ssudo

--- scripts/prerequisites.sh
+++ scripts/prerequisites.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install git btop tmux stow
-sudo ./install_nvim.sh
+apt-get update
+apt-get install git btop tmux stow
+./install_nvim.sh
 
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/14.1.0/ripgrep_14.1.0-1_amd64.deb
-sudo dpkg -i ripgrep_14.1.0-1_amd64.deb
+dpkg -i ripgrep_14.1.0-1_amd64.deb
