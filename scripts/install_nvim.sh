3f39301ccc9ff2a12b6b6bb86b6a7eca4e186f11
Charles-Philippe <c.p.labbe@me.com>
2025-04-09 21:45:18 -0400

    Update install_nvim.sh removed unecessary dir

--- scripts/install_nvim.sh
+++ scripts/install_nvim.sh
@@ -2,6 +2,6 @@
 VERSION=nvim-linux-x86_64
 VERSION_EXT=$VERSION.tar.gz
 wget https://github.com/neovim/neovim/releases/latest/download/$VERSION_EXT
-tar -zxf $VERSION_EXT -C /usr/share/$VERSION
+tar -zxf $VERSION_EXT -C /usr/share
 rm /usr/bin/nvim
 ln -s /usr/share/$VERSION/bin/nvim /usr/bin/nvim
