f85973ebcd07a8e0aa4f8529c54ee67200287709
Charles-Philippe <c.p.labbe@me.com>
2025-04-09 21:59:28 -0400

    Update install_nvim.sh

--- scripts/install_nvim.sh
+++ scripts/install_nvim.sh
@@ -3,5 +3,5 @@ VERSION=nvim-linux-x86_64
 VERSION_EXT=$VERSION.tar.gz
 wget https://github.com/neovim/neovim/releases/latest/download/$VERSION_EXT
 tar -zxf $VERSION_EXT -C /usr/share
-rm /usr/bin/nvim
+rm /usr/bin/nvim || true
 ln -s /usr/share/$VERSION/bin/nvim /usr/bin/nvim
