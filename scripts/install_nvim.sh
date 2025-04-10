6464282dcd0eecf857fc574ffe9ae7ad4bded05e
Charles-Philippe <c.p.labbe@me.com>
2025-04-09 21:30:09 -0400

    Update install_nvim.sh remove sudo

--- scripts/install_nvim.sh
+++ scripts/install_nvim.sh
@@ -2,6 +2,6 @@
 VERSION=nvim-linux-x86_64
 VERSION_EXT=$VERSION.tar.gz
 wget https://github.com/neovim/neovim/releases/latest/download/$VERSION_EXT
-sudo tar -zxf $VERSION_EXT /usr/share/$VERSION
-sudo rm /usr/bin/nvim
-sudo ln -s /usr/share/$VERSION/bin/nvim /usr/bin/nvim
+tar -zxf $VERSION_EXT /usr/share/$VERSION
+rm /usr/bin/nvim
+ln -s /usr/share/$VERSION/bin/nvim /usr/bin/nvim
