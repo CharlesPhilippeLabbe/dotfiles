d0a7ab07b0812a5ebb9ad3f28212b28217d4db5b
Charles-Philippe <c.p.labbe@me.com>
2025-05-22 22:25:21 -0400

    Update prerequisites.sh

--- scripts/prerequisites.sh
+++ scripts/prerequisites.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd $(dirname $0)
-apt-get update
-apt-get install git btop tmux stow
+DEBIAN_FRONTEND=noninteractive apt update && apt install -y git btop tmux stow
 ./install_nvim.sh
 
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/14.1.0/ripgrep_14.1.0-1_amd64.deb
