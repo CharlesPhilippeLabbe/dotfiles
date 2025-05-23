71248547f06036b3567430be53583701d8a47046
Charles-Philippe <c.p.labbe@me.com>
2025-05-22 22:38:09 -0400

    Update install_coretto.sh

--- scripts/install_coretto.sh
+++ scripts/install_coretto.sh
@@ -2,7 +2,7 @@
 sudo apt-get update && sudo apt-get install java-common
 #https://corretto.aws/downloads/latest/amazon-corretto-11-aarch64-linux-jdk.deb
 #ARCH=aarch64
-VERSION=11
+VERSION=$1
 ARCH="x64"
 RESOURCE="amazon-corretto-$VERSION-$ARCH-linux-jdk.deb"
 
